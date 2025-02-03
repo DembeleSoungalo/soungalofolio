0d2b62f96e88cc9c819b1380772ab5e6b70a1148
DembeleSoungalo <dembelesalam27@gmail.com>
2025-02-03 03:30:11 +0100

    fixi sue

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.render(
-  
+  <Analytics>
     <App />
+  </Analytics>
+    
   ,
   document.getElementById("root")
 );
