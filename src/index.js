f6ec3dd47a53fc6cdbd110db1a1983dc70a690fd
DembeleSoungalo <dembelesalam27@gmail.com>
2025-02-03 03:37:37 +0100

    fixing the bug

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.render(
-  <Analytics>
+  
     <App />
-  </Analytics>
-    
   ,
   document.getElementById("root")
 );
