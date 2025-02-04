40c690b85d3a36a2089e739ef127799aa07902b8
Soungalo Salam <dembelesalam27@gmail.com>
2025-02-04 12:03:37 +0000

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,7 @@ import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
 import myImg from "../../Assets/avatar.webp";
+import Tilt from "react-parallax-tilt";
 
 function Home() {
   return (
